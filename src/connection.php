d53b9679e6f854eeef80fb7cf4cdc518d233b7f9
NezirNezirevic01 <70522081+NezirNezirevic01@users.noreply.github.com>
2020-12-09 21:08:09 +0100

    commit

---
+++ src/connection.php
@@ -0,0 +1,16 @@
+<?php
+    ### Connectie variabelen voor db
+    $host = "localhost";  
+    $user = "root";  
+    $password = '';  
+    $db_name = "test";  
+    
+    ### Connectie maken met de database
+    $con = mysqli_connect($host, $user, $password, $db_name);  
+    if(mysqli_connect_errno()) {  
+        die("Gefaald om verbinding te maken met database: ". mysqli_connect_error());  
+    }
+    ### Connectie is gelukt
+    echo "<h1><center>Verbinding met database gelukt</center></h1>";
+?>
+
