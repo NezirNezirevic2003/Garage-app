e25e533904eaf9f5b3c1de8a0663c8d338553e76
NezirNezirevic01 <70522081+NezirNezirevic01@users.noreply.github.com>
2020-12-10 16:27:43 +0100

    commit

--- connection.php
+++ connection.php
@@ -1,16 +1,12 @@
 <?php
-    ### Connectie variabelen voor db
-    $user = "root";  
-    $password = '';  
-    $db_name = "test";  
-    
-    ### Connectie maken met de database
-    $con = mysqli_connect($host, $user, $password, $db_name);  
-    if(mysqli_connect_errno()) {  
-        die("Gefaald om verbinding te maken met database: ". mysqli_connect_error());  
-    }
 
-    ### Connectie is gelukt
-    echo "<h1><center>Verbinding met database gelukt</center></h1>";
-?>
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "garage";
 
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+?> 
--- dbinsert.php
+++ dbinsert.php
@@ -1,3 +1,37 @@
+<?php
+
+include "connection.php";
+
+try {
+    // INSERT query maken en uitvoeren.
+    $stmt = $conn->prepare("INSERT INTO klantgegevens (voornaam, achternaam, stad, zip, adres) 
+    VALUES (:voornaam, :achternaam, :stad, :zip, :adres)");
+    $stmt->bindParam(':voornaam', $voornaam);
+    $stmt->bindParam(':achternaam', $achternaam);
+    $stmt->bindParam(':stad', $stad);
+    $stmt->bindParam(':zip', $zip);
+    $stmt->bindParam(':adres', $adres);
+
+    if (isset($_REQUEST['voornaam']))
+ {
+    $voornaam = $_POST['voornaam'];
+    $achternaam = $_POST['achternaam'];
+    $stad = $_POST['stad'];
+    $zip = $_POST['zip'];
+    $adres = $_POST['adres'];
+    $stmt->execute();
+
+    
+header('Location: dbinsert.php');
+    }}
+    
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+$conn = null;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,35 +42,42 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
-     <form class="needs-validation" novalidate>
+     <form class="needs-validation" method="post" novalidate>
      <div class="form-row">
          <div class="col-md-4 mb-3">
-         <label for="validationCustom01">Voornaam</label>
-         <input type="text" class="form-control" id="validationCustom01" placeholder="Voer uw voornaam in" required>
+               <label for="validationCustom01">Voornaam</label>
+         <input type="text" class="form-control" id="voornaam" name="voornaam"  placeholder="Voer uw voornaam in" required>
          <div class="valid-feedback">
-             Voornaam klopt
+            Voornaam klopt
          </div>
          </div>
          <div class="col-md-4 mb-3">
-         <label for="validationCustom02">Achternaam</label>
-         <input type="text" class="form-control" id="validationCustom02" placeholder="Voer uw achternaam in" required>
+               <label for="validationCustom02">Achternaam</label>
+         <input type="text" class="form-control" id="achternaam" name="achternaam" placeholder="Voer uw achternaam in" required>
          <div class="valid-feedback">
-             Achternaam klopt
+            Achternaam klopt
          </div>
          </div>
            <div class="form-row">
          <div class="col-md-6 mb-3">
-         <label for="validationCustom03">Stad</label>
-         <input type="text" class="form-control" id="validationCustom03" placeholder="Voer de naam van uw stad" required>
+               <label for="validationCustom03">Stad</label>
+         <input type="text" class="form-control" id="stad" name="stad" placeholder="Voer de naam van uw stad" required>
          <div class="invalid-feedback">
-             Stad naam klopt
+            Stad naam klopt
          </div>
          </div>
          <div class="col-md-3 mb-3">
-         <label for="validationCustom05">Zip</label>
-         <input type="text" class="form-control" id="validationCustom05" placeholder="Voer uw zipcode" required>
+               <label for="validationCustom05">Zip</label>
+         <input type="text" class="form-control" id="zip" name="zip" placeholder="Voer uw zipcode" required>
          <div class="invalid-feedback">
-             Zip code klopt
+            Zip code klopt
+         </div>
+         </div>
+         <div class="col-md-3 mb-3">
+               <label for="validationCustom05">Adres</label>
+         <input type="text" class="form-control" id="adres" name="adres" placeholder="Voer uw adres hier" required>
+         <div class="invalid-feedback">
+            Zip code klopt
          </div>
          </div>
      </div>
@@ -50,3 +91,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
+
