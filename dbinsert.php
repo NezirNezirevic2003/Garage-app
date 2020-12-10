9985de72c704732c1c62e777e8efb0de202c2303
NezirNezirevic01 <70522081+NezirNezirevic01@users.noreply.github.com>
2020-12-10 16:34:54 +0100

    commit

--- dbinsert.php
+++ dbinsert.php
@@ -44,6 +44,21 @@ $conn = null;
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Garage</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#expandme">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="expandme" >
+            <div class="navbar-nav test">
+                <a href="./dbinsert.php" class="nav-item nav-link">Create</a>
+                <a href="" class="nav-item nav-link">Read</a>
+                <a href="" class="nav-item nav-link">Update</a>
+                <a href="" class="nav-item nav-link">Delete</a>
+                <a href="" class="nav-item nav-link">Login</a>
+            </div>
+        </div>
+    </nav>
      <form class="needs-validation" method="post" novalidate>
      <div class="form-row">
          <div class="col-md-4 mb-3">
