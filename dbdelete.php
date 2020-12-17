b9db300facf69d9cde4bca762f3e9a9d4c61205a
NezirNezirevic01 <70522081+NezirNezirevic01@users.noreply.github.com>
2020-12-17 11:16:04 +0100

    commit

--- dbdelete.php
+++ dbdelete.php
@@ -1,25 +1,44 @@
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="nl">
 <head>
-<meta name="author"content="Anjo Eijeriks">
-<meta chatset="UTF-8">
-<title>gar-delete-klant1.php</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Klant</title>
 </head>
 <body>
-<h1>garage delete klant 1</h1>
-<p>
-           Dit formulier zoekt een klant op uit
-           de tabel klanten van database garage
-           om hem te kunnen verwijderen.
-</p>
-<form action="dbdelete2.php"method="post">
-            Welk klantid wilt u verwijderen?
-<input type="text"name="klantid"><br/>
-<input type="submit">
-</form>
+    <!-- Navigatie menu -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="./klant.html">Garage</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#expandme">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="expandme" >
+            <div class="navbar-nav test">
+              <a href="./dbinsert.php" class="nav-item nav-link">Create</a>
+              <a href="./dbread.php" class="nav-item nav-link">Read</a>
+              <a href="./dbupdate.php" class="nav-item nav-link">Update</a>
+              <a href="./dbdelete.php" class="nav-item nav-link">Delete</a>
+              <a href="./login.html" class="nav-item nav-link">Login</a>
+              <a href="./auto.html" class="nav-item nav-link">Auto</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Einde navigatie menu -->
+
+    <form action="dbdelete2.php" method="post" class="form-inline">
+       <div class="form-group mx-sm-3 mb-2">
+         <label for="inputPassword2" class="sr-only">Klant-id</label>
+         <input type="text" name="klantidvak" class="form-control" id="inputPassword2" placeholder="Plaats een klant-id hier">
+       </div>
+       <input type="submit" class="btn btn-primary mb-2"></input>
+        </form>
+
+    <!-- Bootstrap scripts -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
-
- 
 
