122521bbccd1b69f4d574f332ca690f67cc56bd7
Anthenny <anthenny1997@hotmail.com>
2021-01-04 19:38:48 +0100

    commit

--- dbupdate2.php
+++ dbupdate2.php
@@ -1,12 +1,29 @@
 <html lang="nl">
 <head>
-    <meta name="author" content="anjo eijeriks">
-    <meta charset="utf-8">
-    <title>gar-update-klant2.php</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update 2</title>
     <link rel="stylesheet" href="./public/style.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
+    <!-- Navigatie menu -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="./public/auto.html">Garage</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#expandme">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="expandme" >
+            <div class="navbar-nav test">
+                <a href="../dbinsertAuto.php" class="nav-item nav-link">Create</a>
+                <a href="../dbreadauto.php" class="nav-item nav-link">Read</a>
+                <a href="" class="nav-item nav-link">Update</a>
+                <a href="" class="nav-item nav-link">Delete</a>
+                <a href="./login.html" class="nav-item nav-link">Login</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Einde Navigatie menu -->
     <h1>garage update klant 2</h1>
     <p>
         dit formulier wordt gebruikt om klantgegevens te wijzigen
--- delete-auto2.php
+++ delete-auto2.php
@@ -1,11 +1,30 @@
 <!doctype html>
 <html lang="nl">
-    <head>
-        <meta name="author" content="Anjo Eijeriks">
-        <meta chatset="UTF-8">
-        <title>gar-delete-auto2.php</title>  
-    </head>
-    <body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete 2</title>
+    <link rel="stylesheet" href="./public/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+    <!-- Navigatie menu -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="./public/auto.html">Garage</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#expandme">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="expandme" >
+            <div class="navbar-nav test">
+                <a href="../dbinsertAuto.php" class="nav-item nav-link">Create</a>
+                <a href="../dbreadauto.php" class="nav-item nav-link">Read</a>
+                <a href="" class="nav-item nav-link">Update</a>
+                <a href="" class="nav-item nav-link">Delete</a>
+                <a href="./login.html" class="nav-item nav-link">Login</a>
+            </div>
+        </div>
+    </nav>
+    <!-- Einde Navigatie menu -->
         <h1>Delete auto 2</h1>
         <p>
            Op autokenteken gegevens zoeken uit de
