4c072116222591b435c252020954c6b88001b05e
Anthenny <anthenny1997@hotmail.com>
2021-01-11 18:49:43 +0100

    commit

--- dbdelete3.php
+++ dbdelete3.php
@@ -1,3 +1,6 @@
+<?php
+require 'connection.php';
+?>
 <!doctype html>
 <html lang="nl">
     <head>
@@ -11,7 +14,7 @@
         <?php
             // gegevens uit het formulier halen
             $klantid = $_POST["klantidvak"];
-            $klanten = $conn->prepare(' SELECT klantid FROM autogegevens WHERE klantid = :klantid');
+            $klanten = $conn->prepare('SELECT klantid FROM autogegevens WHERE klantid = :klantid');
             $klanten->execute(["klantid" => $klantid]);
             // alle data pakken.
             $waarde = $klanten->fetchAll();
