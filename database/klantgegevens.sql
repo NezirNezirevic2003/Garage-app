736f8cd46ddf5129fae6e1dbc8060e6e98a0fd15
Anthenny <anthenny1997@hotmail.com>
2021-01-11 19:53:34 +0100

    commit

--- database/klantgegevens.sql
+++ database/klantgegevens.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Gegenereerd op: 11 jan 2021 om 15:46
--- Serverversie: 10.4.14-MariaDB
--- PHP-versie: 7.4.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `garage`
---
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `klantgegevens`
---
 
 CREATE TABLE `klantgegevens` (
   `klantid` int(11) NOT NULL,
@@ -35,9 +10,6 @@ CREATE TABLE `klantgegevens` (
   `klantplaats` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Gegevens worden geëxporteerd voor tabel `klantgegevens`
---
 
 INSERT INTO `klantgegevens` (`klantid`, `klantnaam`, `klantadres`, `klantpostcode`, `klantplaats`) VALUES
 (1, 'Anthenny', 'Krimpen', '2923EP', 'Zuid-Holland'),
@@ -45,27 +17,9 @@ INSERT INTO `klantgegevens` (`klantid`, `klantnaam`, `klantadres`, `klantpostcod
 (3, 'Nezir', ' Rotterdam', '3531EO', 'Zuid-Holland'),
 (4, 'Mootje', 'Den Helder', '5694FU', 'Noord-Holland');
 
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `klantgegevens`
---
 ALTER TABLE `klantgegevens`
   ADD PRIMARY KEY (`klantid`);
 
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `klantgegevens`
---
 ALTER TABLE `klantgegevens`
   MODIFY `klantid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
