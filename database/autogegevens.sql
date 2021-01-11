6c34071cd122709e0955cadbd157880657e9dc06
Anthenny <anthenny1997@hotmail.com>
2021-01-11 15:48:31 +0100

    commit

--- database/autogegevens.sql
+++ database/autogegevens.sql
@@ -1,18 +1,28 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 CREATE TABLE `autogegevens` (
   `autokenteken` varchar(15) NOT NULL,
   `automerk` varchar(20) NOT NULL,
   `autotype` varchar(255) NOT NULL,
-  `autokmstand` varchar(255) NOT NULL,
-  `klantid` int(255) NOT NULL
+  `autokmstand` int(100) NOT NULL,
+  `klantid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
 INSERT INTO `autogegevens` (`autokenteken`, `automerk`, `autotype`, `autokmstand`, `klantid`) VALUES
-('04-JDK-9', 'Volkswagen', 'Polo', '4087', 3),
-('37-TVR-1', 'Opel', 'Corsa', '80544', 2),
-('67-YB-44', 'Tatra', '603', '309872', 1),
-('95-RP-LR', 'Fiat', 'Doblo', '143989', 1),
-('HV-GB-23', 'Jaguar', 'XJ6', '2043439', 1);
+('04-JDK-9', 'Volkswagen', 'Polo', 4087, 1),
+('14-PR-TX', 'LAMBO', 'gti', 800, 2),
+('37-TVR-1', 'Opel', 'Corsa', 80544, 3),
+('67-YB-44', 'Tatra', '603', 309872, 4),
+('95-RP-LR', 'Fiat', 'Doblo', 143989, 1),
+('99-TT-TX', 'LAMBO', 'GTI', 232, 2),
+('99-XX-TT', 'Volkswagen', 'GTI', 2420000, 2),
+('HV-GB-23', 'Jaguar', 'XJ6', 2043439, 1);
+
+ALTER TABLE `autogegevens`
+  ADD PRIMARY KEY (`autokenteken`),
+  ADD KEY `fk_klantgegevens_autogegevens` (`klantid`) USING BTREE;
 COMMIT;
 
 -- Plakt de volgende line code in autogegevens en dan in sql zodat we een foreign key linken met onze klantgegevens
--- database/klantgegevens.sql
+++ database/klantgegevens.sql
@@ -1,21 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 11 jan 2021 om 15:46
+-- Serverversie: 10.4.14-MariaDB
+-- PHP-versie: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `garage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `klantgegevens`
+--
 
 CREATE TABLE `klantgegevens` (
-  `klantid` int(2) NOT NULL,
+  `klantid` int(11) NOT NULL,
   `klantnaam` varchar(15) NOT NULL,
   `klantadres` varchar(30) NOT NULL,
   `klantpostcode` varchar(6) NOT NULL,
   `klantplaats` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `klantgegevens` (`klantid`, `klantnaam`, `klantadres`, `klantpostcode`, `klantplaats`) VALUES
-(2, 'Jan', 'van Klaveren', '6641KL', 'Rotterdam, Zuid'),
-(3, 'Nezir', 'Testing', '1234TH', 'Testplaats'),
-(4, 'Nezir', 'Test', '1234TH', 'Test');
+--
+-- Gegevens worden geëxporteerd voor tabel `klantgegevens`
+--
 
+INSERT INTO `klantgegevens` (`klantid`, `klantnaam`, `klantadres`, `klantpostcode`, `klantplaats`) VALUES
+(1, 'Anthenny', 'Krimpen', '2923EP', 'Zuid-Holland'),
+(2, 'Ali', 'Rotterdam', '3231VD', 'Zuid-Holland'),
+(3, 'Nezir', ' Rotterdam', '3531EO', 'Zuid-Holland'),
+(4, 'Mootje', 'Den Helder', '5694FU', 'Noord-Holland');
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `klantgegevens`
+--
 ALTER TABLE `klantgegevens`
   ADD PRIMARY KEY (`klantid`);
 
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `klantgegevens`
+--
 ALTER TABLE `klantgegevens`
-  MODIFY `klantid` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `klantid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
